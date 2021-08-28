f727b0416d429a9272d39236c99ebac66cb41ca8
honokame <e1607@s.akashi.ac.jp>
2021-08-29 04:50:53 +0900

    Add files via upload

---
+++ gakusai2021/practice/led_red.py
@@ -0,0 +1,13 @@
+import time
+import phenox as px
+import phenox_params as pxprm
+
+if __name__ == "__main__":
+  pxprm.set_parameter()
+  led_target = px.PX_LED_RED
+  led_is_on = False
+
+  while True:
+    time.sleep(1.0)
+    px.set_led(led_target,led_is_on)
+    led_is_on = not led_is_on
